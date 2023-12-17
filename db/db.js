8ae98d11c1aef975b599ac155e3fa236b276d86d
TaskReminder <21104023@ittelkom-pwt.ac.id>
2023-12-17 20:00:56 +0700

    update db

--- db/db.js
+++ db/db.js
@@ -3,10 +3,11 @@ const Sequelize = require('sequelize');
 const db = new Sequelize(
     'daurdb',
     'daurin',
-    '',
+    'asty',
     {
         host: '35.188.23.160',
         dialect: 'mysql',
+        
     }
 )
 
