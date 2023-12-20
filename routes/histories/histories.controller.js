6dce425785bf9b0acd3a07d259ec5d171c04a939
TaskReminder <21104023@ittelkom-pwt.ac.id>
2023-12-20 23:28:20 +0700

    update

--- routes/histories/histories.controller.js
+++ routes/histories/histories.controller.js
@@ -1,24 +1,23 @@
-const Shop = require('../../models/histories');
 const createHistories = require('../../models/createHistories');
 const History = require('../../models/histories');
 
-
 exports.createHistories = async (req, res) => {
   try {
-    const { History_id, shop_id, id_user, price, status} = req.body;
-    const userId = req.id_user; 
+    const { shop_id, id_user, price, status } = req.body;
+    const userId = req.id_user;
 
-    const newHistory = await createShop(History_id, shop_id, id_user, price, status);
+    // Perbarui nama kolom agar sesuai dengan model dan pastikan tidak ada kolom yang tidak diperlukan
+    const newHistory = await createHistories({ shop_id, id_user, price, status });
 
     return res.status(201).json({ 
       status: 'success', 
       message: 'History created successfully', 
       data: {
-       History_id: newHistory.History_id,
-       shop_id: newHistory.shop_id,
-       id_user: newHistory.id_user,
-       price: newHistory.price,
-       status: newHistory.status,
+        History_id: newHistory.id_history, // Pastikan nama kolom yang benar
+        shop_id: newHistory.shop_id,
+        id_user: newHistory.id_user,
+        price: newHistory.price,
+        status: newHistory.status,
       },
     });
   } catch (error) {
@@ -27,11 +26,11 @@ exports.createHistories = async (req, res) => {
   }
 };
 
-
 exports.getAllHistory = async (req, res) => {
   try {
-    const History = await History.findAll();
-    return res.status(200).json({ status: 'success', message: 'Get all History success', data: History });
+    // Ubah nama variabel untuk menghindari konflik dengan model
+    const histories = await History.findAll();
+    return res.status(200).json({ status: 'success', message: 'Get all History success', data: histories });
   } catch (error) {
     console.error(error);
     res.status(500).json({ status: 'error', message: 'Internal server error' });
@@ -40,9 +39,9 @@ exports.getAllHistory = async (req, res) => {
 
 exports.getHistoryById = async (req, res) => {
   try {
-    const History_id = req.params.historyId;
-    const shop = await Shop.findOne({ where: { History_id: historyId } });
-    return res.status(200).json({ status: 'success', message: 'Get shop by id success', data: history });
+    const historyId = req.params.historyId; // Ubah nama variabel
+    const history = await History.findOne({ where: { id_history: historyId } }); // Sesuaikan dengan nama kolom yang benar
+    return res.status(200).json({ status: 'success', message: 'Get History by id success', data: history });
   } catch (error) {
     console.error(error);
     res.status(500).json({ status: 'error', message: 'Internal server error' });
